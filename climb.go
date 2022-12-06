8ab3b181a92ca5f1c951d0190dae251acda82645
iabdukhoshimov <i.abdukhoshimov@gmail.com>
2022-12-07 01:18:44 +0500

    linked list problem solved

--- climb.go
+++ climb.go
@@ -1,15 +1,14 @@
 package main
 
 func climbStairs(n int) int {
+
 	if n == 1 {
 		return 1
 	}
 	if n == 2 {
 		return 2
 	}
-	var result int
-	for i, j := 1, 2; j <= n; i, j = i+1, j+1 {
-		result = i + j
-	}
-	return result
+
+	return climbStairs(n-1) + climbStairs(n-2)
+
 }
---
+++ removeDuplicate.go
@@ -0,0 +1,12 @@
+package main
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	head.Next = deleteDuplicates(head.Next)
+	if head.Val == head.Next.Val {
+		return head.Next
+	}
+	return head
+}
